846294a7c1a98ec90e5fe3f72e0ad9ea60a113dc
Denil Shaji <xdchathan@gmail.com>
2024-12-14 12:03:57 +0530

    homepage complete

--- lib/view/home/home_scroll_view.dart
+++ lib/view/home/home_scroll_view.dart
@@ -8,3 +8,37 @@ class HomeScrollView extends StatelessWidget {
     return const Placeholder();
   }
 }
+
+class ScrollOne extends StatelessWidget {
+  const ScrollOne(
+      {super.key}); // Added `const` and corrected the class name capitalization
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: SizedBox(
+        height: 300,
+        width: 395,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8),
+              width: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: const DecorationImage(
+                  image: AssetImage(
+                    'assets/1.jpeg',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
